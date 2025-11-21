26db7015c516d7ef0b149db9c0fb1ef845b6b987
HannahVrou <info@hannahbarlindesign.com>
2025-11-21 09:12:08 +0100

    styling + loggor inlagda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import "../styles/global.css";
 import React from "react";
-import LogoCircle from "../assets/images/logos/kiwision-round-07.png"
+import LogoCircle from "../assets/images/logos/round_logo-kiwision-12.png"
 
 export default function Footer() {
   return (
-    <footer data-name="footer" className="footer" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-      <img src={LogoCircle} alt="Kiwision Logo" style={{ maxHeight: "60px", marginBottom: "10px" }} />
-      <p style={{fontWeight:"200"}}>Kiwision © {new Date().getFullYear()} All rights reserved</p>
+    <footer data-name="footer" className="footer" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "start"}}>
+      <img src={LogoCircle} alt="Kiwision Logo" style={{ maxHeight: "60px", marginLeft: "2rem", marginRight: "2rem" }} />
+      <p style={{fontFamily: "Arboria, sans-serif", fontWeight:"400"}}>Kiwision © {new Date().getFullYear()} All rights reserved</p>
     </footer>
   );
 }
